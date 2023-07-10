06525b8e1543cf926dd83a424abbabc6bd5faa65
Jaraslau Babich <j.babich@paralect.com>
2023-07-10 08:33:51 +0200

    add fly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+  const repo = await getNextStargazers()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -37,6 +39,7 @@ export default function Home() {
           height={37}
           priority
         />
+        Stars: {repo.stargazers_count}
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -110,4 +113,14 @@ export default function Home() {
       </div>
     </main>
   )
+}
+
+type Repo = {
+  name: string
+  stargazers_count: number
+}
+
+const getNextStargazers = async (): Promise<Repo> => {
+  const res = await fetch('https://api.github.com/repos/vercel/next.js')
+  return await res.json()
 }
