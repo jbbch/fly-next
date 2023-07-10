38039c59df5ca6922aa5fc57c3c557f4bb412245
Jaraslau Babich <j.babich@paralect.com>
2023-07-10 08:59:44 +0200

    check tsc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 export default async function Home() {
   const repo = await getNextStargazers()
 
+  console.log({ repo: repo.test })
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
