62fdf3c505bf67877509993890b9c4f7048e798f
jbbch <1733285+jbbch@users.noreply.github.com>
2024-10-23 13:30:09 +0000

    some breaking change to test coderabbit

--- prisma/migrations/20230712195740_init/migration.sql
+++ prisma/migrations/20230712195740_init/migration.sql
@@ -8,7 +8,7 @@ CREATE TABLE "User" (
 -- CreateTable
 CREATE TABLE "Post" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
+    "title" TEXT NOT NULL,,
     "content" TEXT,
     "published" BOOLEAN NOT NULL DEFAULT false,
     "authorId" INTEGER NOT NULL,
